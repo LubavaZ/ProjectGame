5fd2198a56d7cd345c5c76a0900492d6a35a41a1
LubavaZ <lubavazigalko@gmail.com>
2021-09-01 16:25:35 +0300

    new

--- js/script.js
+++ js/script.js
@@ -9,11 +9,11 @@ canvas.width = widthOfCan;
 canvas.height = heightOfCan;
 
 const appleImg = new Image();
-appleImg.src = 'images/apple.png';
+appleImg.src = './images/apple.png';
 const bananaImg = new Image();
 bananaImg.src = './images/banana.png';
 const bombImg = new Image();
-bombImg.src = '../../images/bomb.png'
+bombImg.src = './images/bomb.png'
 
 const soundOfFood = new Audio();
 soundOfFood.src = '../sounds/soundOfFood.mp3';
